2aedeb8bbb67c9a12d33353c911a778d30d2cc72
qe <qiuyifeng1997@outlook.com>
2024-01-08 17:57:17 +1100

    fix: remove junk

--- docs/publish.sh
+++
@@ -1,8 +0,0 @@
-git checkout -b pub
-git reset --hard master
-bundle exec jekyll build
-git add .
-git commit -m "chore: generate a static site"
-git push --set-upstream origin pub
-
-git checkout master
